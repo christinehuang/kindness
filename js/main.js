aa67f08770ef4bfece3dbfd2569a41e32ca6a6d3
chris <chinyu0723@gmail.com>
2019-03-06 13:41:04 +0800

    reserv-detail desktop

--- js/main.js
+++ js/main.js
@@ -61,9 +61,6 @@ $(document).ready(function(){
     colorEnd: 'Pink'
   })//this is requires a plugin
   .addTo(controller);
-
-
-
  
 
 //this is news
@@ -311,63 +308,6 @@ $(document).ready(function(){
   })//this is requires a plugin
   .addTo(controller);
   });
-
- var ourScene8 = new ScrollMagic.Scene({
-      triggerElement: '#blackbg',
-      triggerHook: 0,
-      reverse: true
-  })
-  .setClassToggle('#navbar', 'black') //add class to section1-pic
-  .addIndicators({
-    name: 'changebg nav',
-    colorTrigger: '#c90',
-    colorStart: '#F60',
-    colorEnd: 'Pink',
-    indent: 400
-  })//this is requires a plugin
-  .addTo(controller);
-
-    var ourScene9 = new ScrollMagic.Scene({
-      triggerElement: '#blackbg',
-      triggerHook: 0,
-      reverse: true
-  })
-  .setClassToggle('#nav-logo-b', 'dis') //add class to section1-pic
-  .addIndicators({
-    name: 'changebg nav',
-    colorTrigger: '#c90',
-    colorStart: '#F60',
-    colorEnd: 'Pink'
-  })//this is requires a plugin
-  .addTo(controller);
-
-  var ourScene10 = new ScrollMagic.Scene({
-      triggerElement: '#blackbg',
-      triggerHook: 0,
-      reverse: true
-  })
-  .setClassToggle('#nav-logo-w', 'show') //add class to section1-pic
-  .addIndicators({
-    name: 'changebg nav',
-    colorTrigger: '#c90',
-    colorStart: '#F60',
-    colorEnd: 'Pink'
-  })//this is requires a plugin
-  .addTo(controller);
-
-  var ournavScene = new ScrollMagic.Scene({
-      triggerElement: '#blackbg',
-      triggerHook: 0,
-      reverse: true
-  })
-  .setClassToggle('#index-1200', 'white') //add class to section1-pic
-  .addIndicators({
-    name: 'changenavbg',
-    colorTrigger: 'red',
-    colorStart: '#F60',
-    indent:300
-  })//this is requires a plugin
-  .addTo(controller);
 
 
 });
